f0eb16bd2998a4e2fdbe394989fc1cd358ee92b0
uwDavid <uwDavid@users.noreply.github.com>
2024-06-22 20:43:32 -0400

    todo - completed

--- ToDoApp/Models/ToDoItem.cs
+++ ToDoApp/Models/ToDoItem.cs
@@ -4,6 +4,18 @@ public class ToDoItem
 {
     public int Id { get; set; }
     public string Name { get; set; } = "";
-    public bool IsCompleted { get; set; }
+    public bool _isCompleted;
+    public bool IsCompleted
+    {
+        get => _isCompleted;
+        set
+        {
+            _isCompleted = value;
+            if (value)
+            {
+                DateCompleted = DateTime.Now;
+            }
+        }
+    }
     public DateTime DateCompleted { get; set; }
 }
